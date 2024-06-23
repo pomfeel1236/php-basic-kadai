8d8f26df7a18f67c0fbe5febc86b0296a86d498d
pomfeel1236 <158986613+pomfeel1236@users.noreply.github.com>
2024-06-24 04:17:24 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $product_list = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($product_list);
+
+    ?>
+  </p>
+</body>
+
+</html>
