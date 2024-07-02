6669d21b8427e88cf42b6bf3fb58c0c7cdaaea37
pomfeel1236 <158986613+pomfeel1236@users.noreply.github.com>
2024-07-02 10:52:23 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    class Food
+    {
+      //プロパティを定義する
+      private $name;
+      private $price;
+      //メソッドを定義
+      public function get_price()
+      {
+        $this->price = 250;
+        echo $this->price . '<br>';
+      }
+      //コンストラクタの初期化
+      public function __construct(string $name, int $price)
+      {
+        $this->name = $name;
+        $this->price = $price;
+      }
+    }
+
+    //インスタンス化
+    $food = new Food('potato', 250);
+
+    //メソッドにアクセスしメソッドを実行
+    print_r($food);
+    echo '<br>';
+
+    //クラスの定義
+    class Animal
+    {
+      //プロパティを定義する
+      private $name;
+      private $height;
+      public $weight;
+      //メソッドの定義
+      public function get_height()
+      {
+        $this->height = 60;
+        echo $this->height . '<br>';
+      }
+      //コンストラクタの初期化
+      public function __construct(string $name, int $height, float $weight)
+      {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+      }
+    }
+
+    //インスタンス化
+    $animal = new Animal('dog', 60, 5000);
+    //インスタンス$animalに各プロパティの値を出力する
+    print_r($animal);
+    echo '<br>';
+
+    $food->get_price();
+    $animal->get_height();
+    ?>
+  </p>
+</body>
+
+</html>
